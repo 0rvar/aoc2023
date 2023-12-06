62c6d80079f15a6b66a8fe74359a5e5fb381a6d8
Orvar Segerström <orvarsegerstrom@gmail.com>
2023-12-06 07:42:11 +0100

    Day 6 part 2

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -4,28 +4,22 @@ fn main() {
     let mut aoc = initialize_aoc();
     let input = aoc.input();
 
-    //     let input = "
-    // Time:      7  15   30
-    // Distance:  9  40  200"
-    //         .trim();
-
     aoc.measure("Parsing");
 
     let (time, distance) = input.split_once('\n').unwrap();
-    let times = time
-        .split_once(':')
-        .unwrap()
-        .1
+    let times_str = time.split_once(':').unwrap().1;
+    let record_distances_str = distance.split_once(':').unwrap().1;
+
+    let times = times_str
         .split_whitespace()
         .map(|x| x.parse::<u64>().unwrap());
-    let record_distances = distance
-        .split_once(':')
-        .unwrap()
-        .1
+    let record_distances = record_distances_str
         .split_whitespace()
         .map(|x| x.parse::<u64>().unwrap());
 
     let races = times.zip(record_distances).collect::<Vec<_>>();
+
+    aoc.measure("Part 1");
 
     let margin_of_error = races
         .iter()
@@ -42,6 +36,30 @@ fn main() {
             num_winning_strategies
         })
         .product::<u64>();
-
     tracing::info!("Part 1: {margin_of_error}");
+
+    aoc.measure("Part 2 parsing");
+    let race_time = times_str
+        .split_whitespace()
+        .collect::<String>()
+        .parse::<u64>()
+        .unwrap();
+    let race_record = record_distances_str
+        .split_whitespace()
+        .collect::<String>()
+        .parse::<u64>()
+        .unwrap();
+
+    aoc.measure("Part 2");
+
+    let mut num_winning_strategies = 0;
+
+    for hold_time in 1..race_time {
+        let speed = hold_time;
+        if speed * (race_time - hold_time) > race_record {
+            num_winning_strategies += 1;
+        }
+    }
+
+    tracing::info!("Part 2: {num_winning_strategies}");
 }
